1b39041b7214ace905cbebb02df2a76772936e75
Linguagem de Máquina <contato@linguagemdemaquina.com.br>
2022-11-30 20:49:03 -0300

    JAVASCRIPT - MODELOS DE OPERAÇÕES COM DATAS

--- PADRÕES/JS/datas.js
+++ PADRÕES/JS/datas.js
@@ -1,2 +1,28 @@
-const data_atual = new Date();
-console.log(data_atual);
+// 1 - OBTENDO DATA COMPLETA ATUAL
+
+const dataAtual = new Date();
+
+// 2 - OBTENDO NOME DO DIA DA SEMANA ATUAL 
+
+const diaAtual = new Date ();
+const numeroDodiaatual = diaAtual.getDay();
+    // ARRAY COM NOMES DOS DIAS DA SEMANA
+    var arrayDiadasemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    // VINCULA NOME DO DIA NO ARRAY
+    var nomeDodia = arrayDiadasemana[numeroDodiaatual];
+
+
+// 3 - OBTENDO NOME DO MÊS ATUAL
+
+const mesAtual = new Date ();
+const numeroDomesatual = mesAtual.getMonth();
+    // ARRAY COM NOMES DOS MESES
+    var arrayMesesdoano = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho','Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    // VINCULA NOME DO DIA NO ARRAY
+    var nomeDomes = arrayMesesdoano[numeroDomesatual];
+
+
+// 4 - EXTRAINDO O ANO ATUAL COM 4 DÍGITOS
+
+const anoAtual = new Date ();
+const valorDoanoatual = anoAtual.getFullYear();
