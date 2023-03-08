65d849259ed0671ab942d82f0a3e6e969fc46c6c
Linguagem de Máquina <contato@linguagemdemaquina.com.br>
2023-03-07 23:23:01 -0300

    Padronização de código básico

--- PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_1.py
+++ PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_1.py
@@ -1,15 +1,23 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+# Sub-Classe que define padrões da tela principal
+class home(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Aplicativo 1 Teste - Python")
+        self.setStyleSheet('background-color: #666666')
+        self.resize(300,600)
+  
 
 aplicativo = QApplication(sys.argv)
 
-home = QWidget()
-home.resize(300,600)
-home.setWindowTitle("Aplicativo 1 - Python")
-home.setStyleSheet('background-color: #666666')
-home.show()
-
-
+janelaInicial = home()
+janelaInicial.show()
 
 aplicativo.exec()
