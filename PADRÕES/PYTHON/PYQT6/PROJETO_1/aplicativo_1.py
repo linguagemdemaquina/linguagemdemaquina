765277f67a84b708c68791539a64cbea718227a5
Linguagem de Máquina <contato@linguagemdemaquina.com.br>
2023-03-08 22:50:04 -0300

    Adequação de padrões

--- PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_1.py
+++ PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_1.py
@@ -1,15 +1,32 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt6.QtCore import Qt 
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QVBoxLayout, QMainWindow
 
 aplicativo = QApplication(sys.argv)
 
-home = QWidget()
-home.resize(300,600)
-home.setWindowTitle("Aplicativo 1 - Python")
-home.setStyleSheet('background-color: #666666')
+class janelaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        #Define as características da janela principal
+        self.setWindowTitle('Aplicativo 1 - Python PyQt6')
+        self.resize(300,600)
+        self.setStyleSheet('background-color: #666666; color:#FFFFFF;')
+        
+        
+        #Define o título da seção e suas características
+        titulo = QLabel(self)
+        titulo.setText('Programando com Python - PyQT6')
+        titulo.setStyleSheet('margin-top:5px; font-size:14px; font-weight:bold;')
+        titulo.show()
+
+        self.setCentralWidget(titulo)
+        titulo.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        
+
+
+home = janelaPrincipal()
 home.show()
-
-
 
 aplicativo.exec()
