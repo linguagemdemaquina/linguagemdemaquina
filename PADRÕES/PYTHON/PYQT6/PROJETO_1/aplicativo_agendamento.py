c20d21a1040d482259649b106a94bd030bd9288d
Linguagem de Máquina <contato@linguagemdemaquina.com.br>
2023-03-20 23:49:54 -0300

    Ajustes e inclusão de recursos qss

--- PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_agendamento.py
+++ PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_agendamento.py
@@ -1,108 +1,71 @@
 # Powered by Linguagem de Máquina using PyQT6
-# =========================================================
+# ==========================================================
 
 # Resources and documentations : 
 # https://www.riverbankcomputing.com/static/Docs/PyQt6/ 
-# =========================================================
+# ==========================================================
 
 import sys
-
+from pathlib import Path
+from PyQt6.QtWidgets import QApplication,  QWidget, QLabel, QLineEdit, QPushButton,  QVBoxLayout
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QCursor
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QPushButton, QLineEdit, QCalendarWidget
+from PyQt6.QtGui import QIcon, QPixmap
 
 
-# Cria o aplicativo
-aplicativo = QApplication(sys.argv)
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle('SISTEMA DE GESTÃO')
+        self.setWindowIcon(QIcon('./imagens/logomarca.png'))
+        self.setFixedWidth(300)
+        self.setFixedHeight(600)
 
-# Características da janela
-# =========================================================
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.setContentsMargins(10,0,10,220)
+        layout.setObjectName('layout')
+        
+        imagemLogomarca = QLabel()
+        logomarcaGrande = QPixmap('./imagens/logomarca_horizontal.png')
+        imagemLogomarca.setPixmap(logomarcaGrande)
+        imagemLogomarca.setObjectName('imagemLogomarcaHorizontal')
 
-# Define a janela como um widjet
-janela =  QWidget()
+        tituloDeSecao = QLabel('LOGIN DE SISTEMA')
+        tituloDeSecao.setObjectName('tituloDeSecao')
 
-# Define o título da janela 
-janela.setWindowTitle("SISTEMA DE AGENDAMENTO")
-# Define o tamanho da janela
-janela.resize(400,600)
+        
+        tituloEmail = QLabel ('EMAIL')
+        tituloEmail.setObjectName('tituloEmail')
+        
+        email = QLineEdit(self)
+        email.setPlaceholderText('DIGITE SEU EMAIL')
+        email.setObjectName('email')
 
+        tituloSenha = QLabel('SENHA')
+        tituloSenha.setObjectName('tituloSenha')
 
-# Caracteríticas do conteúdo da janela
-# =========================================================
+        senha = QLineEdit(self)
+        senha.setEchoMode(QLineEdit.EchoMode.Password)
+        senha.setPlaceholderText('DIGITE SUA SENHA')
+        senha.setObjectName('senha')
+        
+        botao_login = QPushButton('LOGIN')
+        botao_login.setObjectName('botaoLogin')
 
-# Label de título da seção
-label_titulo_secao = QLabel ("SISTEMA DE AGENDAMENTO")
-# Alinha o label no centro
-label_titulo_secao.setAlignment(Qt.AlignmentFlag.AlignCenter)
-# Atribui o CSS aplicável
-label_titulo_secao.setStyleSheet('font-size:16px; font-weight:bold; margin-top:10px; margin-bottom:10px; color:#3366FF;')
+        layout.addWidget(imagemLogomarca)
+        layout.addWidget(tituloDeSecao)        
+        layout.addWidget(tituloEmail)
+        layout.addWidget(email)
+        layout.addWidget(tituloSenha)
+        layout.addWidget(senha)
+        layout.addWidget(botao_login)
 
-
-# Label do campo de entrada de dados - nome
-label_cpf =  QLabel("DIGITE SEU CPF :")
-# Alinha o label no centro
-label_cpf.setAlignment(Qt.AlignmentFlag.AlignCenter)
-# Atribui o CSS aplicável
-label_cpf.setStyleSheet('font-size:12px; font-weight:bold; margin-top:10px; margin-bottom:10px; color:#3366FF;')
-
-# Campo de entrada de dados - cpf
-cpf =  QLineEdit()
-# Define um tamanho fixo para o campo de entrada de dados
-cpf.setFixedWidth(100)
-
-
-# Label do campo de entrada de dados - calendario
-label_calendario =  QLabel("ESCOLHA A DATA DE AGENDAMENTO : ")
-# Alinha o label no centro
-label_calendario.setAlignment(Qt.AlignmentFlag.AlignCenter)
-# Atribui o CSS aplicável
-label_calendario.setStyleSheet('font-size:12px; font-weight:bold; margin-top:10px; margin-bottom:10px; color:#3366FF;')
-
-# Campo de entrada de dados - calendario
-calendario =  QCalendarWidget()
-# Define um tamanho fixo para o calendario
-calendario.setFixedSize(350,400)
-
-# Botão de processamento de dados
-# ========================================================
-
-# Define o título do botão
-botao_ver_agenda =  QPushButton("VER AGENDA")
-# Define um tamanho fixo para o botão
-botao_ver_agenda.setFixedWidth(120)
-# Define o cursor sobre o botão
-botao_ver_agenda.setCursor(QCursor(Qt.CursorShape.ArrowCursor.PointingHandCursor))
-# Atribui o CSS aplicável
-botao_ver_agenda.setStyleSheet('font-weight:bold; padding:5px; margin-top:15px; margin-bottom:15px; color:#3366FF;')
-
-# Composição do layout
-# =========================================================
-
-# Define o layout vertical (VBoxLayout)
-layout =  QVBoxLayout()
-
-# Estrutura os elementos de layout
-# =========================================================
-
-layout.addWidget(label_titulo_secao)
-layout.addWidget(label_cpf)
-# O parâmetro alignment define a posição do campo de entrada de dados - cpf
-layout.addWidget(cpf, alignment=Qt.AlignmentFlag.AlignCenter)
-layout.addWidget(label_calendario)
-# O parâmetro alignment define a posição do calendario
-layout.addWidget(calendario, alignment=Qt.AlignmentFlag.AlignCenter)
-# O parâmetro alignment define a posição do botão
-layout.addWidget(botao_ver_agenda, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.show()
 
 
-# Integra o layout montado à janela
-# =========================================================
-janela.setLayout(layout)
-
-# Exibe a janela
-# =========================================================
-janela.show()
-
-# Executa o aplicativo
-# =========================================================
-aplicativo.exec()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    
+    app.setStyleSheet(Path('.\\qss\estilo.qss').read_text())
+    window = MainWindow()
+    sys.exit(app.exec())
