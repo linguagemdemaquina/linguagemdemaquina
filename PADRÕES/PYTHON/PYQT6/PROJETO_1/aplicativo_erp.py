7b93a3dd735e28605650bc22b4ee56613f32a292
Linguagem de Máquina <contato@linguagemdemaquina.com.br>
2023-04-09 10:11:52 -0300

    AJUSTES DE CONTEÚDO

--- PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_erp.py
+++ PADRÕES/PYTHON/PYQT6/PROJETO_1/aplicativo_erp.py
@@ -6,6 +6,7 @@
 # ==========================================================
 
 import sys
+import mysql.connector
 import os
 
 from pathlib import Path
@@ -23,10 +24,33 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon, QPixmap, QCursor
 
 
-class MainWindow(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setWindowTitle('SISTEMA DE GESTÃO INTEGRADA')
+class TeladeLogin(QWidget):
+    def __init__(self, parent=None):
+        super(TeladeLogin,self).__init__(parent)
+        self.setWindowTitle('ERP - TELA DE LOGIN')
+        self.setWindowIcon(QIcon(caminho_logomarca))
+        self.setFixedWidth(300)
+        self.setFixedHeight(500)
+
+        layout_login = QVBoxLayout()
+        layout_login.setContentsMargins(10,10,10,10)
+        layout_login.setObjectName('layout_login')
+        self.setLayout(layout_login)
+
+        imagemLogomarca = QLabel()
+        logomarcaGrande = QPixmap(caminho_logomarca_horizontal)
+        imagemLogomarca.setPixmap(logomarcaGrande)
+        imagemLogomarca.setObjectName('imagemLogomarcaHorizontal')
+        imagemLogomarca.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # ESTRUTURAÇÃO DO LAYOUT - TELA DE LOGIN
+        layout_login.addWidget(imagemLogomarca)
+
+
+class TelaPrincipal(QWidget):
+    def __init__(self, parent=None):
+        super(TelaPrincipal,self).__init__(parent)
+        self.setWindowTitle('ERP - SISTEMA DE GESTÃO INTEGRADA')
         self.setWindowIcon(QIcon(caminho_logomarca))
         self.setFixedWidth(300)
         self.setFixedHeight(500)
@@ -66,10 +90,42 @@ class MainWindow(QWidget):
         senha.setEchoMode(QLineEdit.EchoMode.Password)
         senha.setPlaceholderText('DIGITE SUA SENHA')
         senha.setObjectName('senha')
+
         
+        #ROTINA DE LOGIN
+        def efetuaLogin():
+            valorEmail = email.text()
+            valorSenha = senha.text()
+
+            # CONECTA A UM BANCO DE DADOS MYSQL
+            mydb = mysql.connector.connect(
+            host="localhost",
+            user="adm_agendamento",
+            password="1234",
+            database="erp_pyqt6"
+            )
+
+            # DEFINE A BUSCA SQL PARA LOGIN
+            busca = mydb.cursor()
+            sql = "SELECT * FROM usuarios WHERE email= '"+ valorEmail +"' AND senha= '" + valorSenha + "'"
+            
+            busca.execute(sql)
+            resultado = busca.fetchall()
+            quantidade = busca.rowcount
+
+            # VALIDA O USUÁRIO        
+            print(quantidade)
+            if quantidade == 1 :
+                print("Usuário logado")
+                print (resultado)
+            else:
+                print("Usuário não logado")
+            
+
         botao_login = QPushButton('LOGIN')
         botao_login.setObjectName('botaoLogin')
         botao_login.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        botao_login.clicked.connect(efetuaLogin)
 
         botaoInstagram = QPushButton('INSTAGRAM')
         instagram = QIcon(QPixmap(caminho_imagem_instagram))
@@ -77,9 +133,7 @@ class MainWindow(QWidget):
         botaoInstagram.setObjectName('botaoInstagram')
         botaoInstagram.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
 
- 
-
-              
+        # ESTRUTURAÇÃO DO LAYOUT - TELA PRINCIPAL
         layout.addWidget(imagemLogomarca)
         layout.addWidget(tituloDeSecao)        
         layout.addWidget(tituloEmail)
@@ -89,13 +143,13 @@ class MainWindow(QWidget):
         layout.addWidget(botao_login, alignment=Qt.AlignmentFlag.AlignHCenter)
         layout.addWidget(botaoInstagram, alignment=Qt.AlignmentFlag.AlignHCenter)
 
-        self.show()
 
-
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
-    
-    
     app.setStyleSheet(Path(caminho_qss).read_text())
-    window = MainWindow()
+    main = TelaPrincipal()
+    main.show()
     sys.exit(app.exec())
+ 
+if __name__ == '__main__':
+    main()
